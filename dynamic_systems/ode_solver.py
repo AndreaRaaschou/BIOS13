fb48b40e157d285d10e0a2f429fe6c43f4934792
Andrea Raaschou <andrea.raaschou@gmail.com>
2025-11-20 15:07:59 +0100

    make ode_solver in python

---
+++ dynamic_systems/ode_solver.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 20 14:58:13 2025
+@author: andrearaaschou
+"""
+import numpy as np
+import  matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+# make function ode_solver
+ode_solver(f, h, x_max):
+    # Make dataframe to hold x- and y-values
+    
+
+# make functions to test it on
+# Find built in function to compare with (use scipy integrate solve_ivp)
+# plot both solutions in the same blot as different lines
