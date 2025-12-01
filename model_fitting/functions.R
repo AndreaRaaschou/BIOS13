3ea9529d02c83e59b74adddb20a0320314797ff6
Andrea Raaschou <andrea.raaschou@gmail.com>
2025-12-01 10:57:28 +0100

    finish model fitting exercise

--- model_fitting/functions.R
+++ model_fitting/functions.R
@@ -10,7 +10,8 @@ calc_ntplus1 <- function(nt, p){
   
   eps = rnorm(1, mean = 0, sd = sd)
   
-  nt_plus1=nt*exp(r0*(1-nt/K) + eps)
+  nt_plus1 = nt * exp (r0 * (1 - nt/K) + eps)
+  
   return(nt_plus1)
 }
 
@@ -22,9 +23,11 @@ one_sim <- function(n0, p){
   # Vector with initial value n0 and length 100
   s <- c(n0, rep(0, 99))
   
-  for (i in 1:99){
+  for (i in 2:100){
     # Add next population size to m
-    s = c(s, calc_ntplus1(s[i], p))
+    s[i] = calc_ntplus1(s[i-1], p)
+    # Do not continue with the simulation if the population goes extinct
+    if (s[i] < 2) break
   }
   return(s)
 }
@@ -38,11 +41,10 @@ mult_sim <- function(n0, p){
   m = matrix(one_sim(n0, p), ncol = 1)
   
   # Repeat and add more simulations 
-  for (i in 2:1000){
+  for (i in 2:1000){ 
     m = cbind(m, one_sim(n0, p))
   }
-  
-  plot_first20(m[, 1:20])
+  return(m)
 }
 
 #' Plot the first 20 simulations in one single plot
@@ -53,6 +55,8 @@ plot_first20 <- function(m){
        xlab = "Years",
        ylab = "Population size",
        main = "20 simulations for 100 years of Ringlets population")
+  # Add a line for the extinction limit of 2
+  abline(h = 2, col = 'red')
   
   for (i in 1:20){
     # loop through m to add lines for the different simulations
@@ -60,7 +64,62 @@ plot_first20 <- function(m){
   }
 }
 
+#' Calculate extinction risk for the given matrix with simulations
+extinction_risk <- function(m){
+  count <- 0
+  
+  # Loop through all columns in m
+  for (i in 1:ncol(m)){
+    # If the population is below zero in the current simulation, add 1 to count
+    if (any(m[,i] < 2)){
+      count = count + 1
+    }
+  }
+  # Return the proportion of simulations that went extinct (risk of extinction)
+  return(count / ncol(m))
+}
 
+#' Plot extinction risk for different parameter values
+plot_extinction_risk <- function(n0, p){
+  # Change r0
+  r0 = seq(1, 2, length.out = 100)
+  r0_ext = rep(0, 100)
+  for (i in 1:100){
+    p[1] = r0[i]
+    m = mult_sim(n0, p)
+    r0_ext[i] = extinction_risk(m)
+  }
+  
+  # Change K, change the starting population size to match K
+  K = seq(10, 400, length.out = 100)
+  K_ext = rep(0, 100)
+  for (i in 1:100){
+    p[2] = K[i]
+    m = mult_sim(K[i], p)
+    K_ext[i] = extinction_risk(m)
+  }
+  
+  # Change sd
+  sd = seq(0.2, 0.8, length.out = 100)
+  sd_ext = rep(0, 100)
+  for (i in 1:100){
+    p[3] = sd[i]
+    m = mult_sim(n0, p)
+    sd_ext[i] = extinction_risk(m)
+  }
+  
+  
+  par(mfrow = c(1, 3))
+  plot(r0, r0_ext, las = 0,
+       xlab = "Intrinsic growth rate (r0)", 
+       ylab = "Extinction risk")
+  plot(K, K_ext, las = 0, 
+       xlab = "Carrying capacity (K)", 
+       ylab = "Extinction risk")
+  plot(sd, sd_ext, las = 0, 
+       xlab = "Standard deviation", 
+       ylab = "Extinction risk")
+}
 
 
 
--- model_fitting/mc_simulations.R
+++ model_fitting/mc_simulations.R
@@ -1,20 +1,20 @@
 # Simulate the Ringlets dynamics 100 years into the future
-# Use last known population size as start value
-# Use the parameter values of chosen model
-# Run 1000 simulations
-# Plot the first 20 years in one single plot
-# Estimate risk of extinction (extinction = N < 2)
-# Try to alter parameter values - see where the population becomes sensitive
+rm(list = ls())
 source("functions.R")
 
-# List of estimated parameter values 
-p = c(r0 = 1.004, K = 311, sd = 0.3445)
+# List of estimated parameter values (r0, K, sd)
+p = c(1.004, 311, 0.3445)
 
-mult_sim(300, p) # Something is very off with the simulations
+# Make multiple simulations
+m <- mult_sim(300, p) 
 
+# Plot the first 20 simulations
+plot_first20(m[, 1:20])
 
-# To do: 
-# Check what is going wrong
-# Make function that calculates extinction risk
-# Also plot extinction risk
+# Calculate and print extinction risk for current parameter values
+ext_risk <- extinction_risk(m)
+print(paste("The extinction risk is:", ext_risk))
+
+# Plot extinction risk for different parameter values
+plot_extinction_risk(300, p)
 
